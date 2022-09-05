3968e090c947e42bc9f91209204c6f8ef3a9cdf1
rohan1993 <rohan93kumar@gmail.com>
2022-09-05 22:41:29 +0545

    Create jenkinsfile.py
    new file

---
+++ jenkinsfile.py
@@ -0,0 +1,15 @@
+pipeline {
+  agent any
+  stages {
+    stage('version') {
+      steps {
+        bat 'python --version'
+      }
+    }
+    stage('hello') {
+      steps {
+        bat 'python test.py'
+      }
+    }
+  }
+}
